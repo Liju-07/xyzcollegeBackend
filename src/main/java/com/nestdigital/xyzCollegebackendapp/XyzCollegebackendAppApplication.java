6d1a8414d9a080cddd45aae14eeb2da5299712d6
Liju-07 <lijusaju03@gmail.com>
2022-10-23 10:12:32 +0530

    xyz

---
+++ src/main/java/com/nestdigital/xyzCollegebackendapp/XyzCollegebackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.xyzCollegebackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzCollegebackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzCollegebackendAppApplication.class, args);
+	}
+
+}
